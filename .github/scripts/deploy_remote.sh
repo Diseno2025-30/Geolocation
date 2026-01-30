caf9572870ae92214f475a609b4a0abc925fc32e
Oliver Ardila <odardilacueto@gmail.com>
2026-01-29 23:44:54 -0500

    Add CORS headers to NGINX configuration for improved API accessibility

--- .github/scripts/deploy_remote.sh
+++ .github/scripts/deploy_remote.sh
@@ -210,6 +210,17 @@ server {
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto $scheme;
         rewrite ^/osrm/(.*) /$1 break;
+
+        add_header 'Access-Control-Allow-Origin' '*' always;
+        add_header 'Access-Control-Allow-Methods' 'GET, OPTIONS' always;
+        
+        if ($request_method = 'OPTIONS') {
+            add_header 'Access-Control-Allow-Origin' '*';
+            add_header 'Access-Control-Max-Age' 1728000;
+            add_header 'Content-Type' 'text/plain charset=UTF-8';
+            add_header 'Content-Length' 0;
+            return 204;
+        }
     }
 }
 NGINXTEMPCONF
@@ -360,10 +371,19 @@ server {
         rewrite ^/osrm/(.*) /\$1 break;
         
         # CORS para Leaflet
-        add_header Access-Control-Allow-Origin "*";
+        add_header 'Access-Control-Allow-Origin' '*' always;
         add_header Access-Control-Allow-Methods "GET, POST, OPTIONS";
         add_header Access-Control-Allow-Headers "DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range";
         
+        if ($request_method = 'OPTIONS') {
+            add_header 'Access-Control-Allow-Origin' '*';
+            add_header 'Access-Control-Max-Age' 1728000;
+            add_header 'Content-Type' 'text/plain charset=UTF-8';
+            add_header 'Content-Length' 0;
+            return 204;
+        }
+
+
         # Anti-cache para OSRM
         add_header Cache-Control "no-store, no-cache, must-revalidate" always;
         add_header Pragma "no-cache" always;
